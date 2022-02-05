d25375ffa83c59d1166d79b8541f41f536637295
lijing18 <lijing18@360.cn>
2022-02-05 09:48:10 +0800

    feat: add transition

--- hello-vue3/src/router/index.js
+++ hello-vue3/src/router/index.js
@@ -1,25 +1,31 @@
 import {
-  createRouter, createWebHashHistory
+    createRouter, createWebHashHistory
 } from 'vue-router';
 import Home from '../pages/home.vue';
 import About from '../pages/about.vue';
+import Test from '../pages/test.vue';
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About,
-  },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About,
+    },
+    {
+        path: '/test',
+        name: 'Test',
+        component: Test,
+    },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(),
+    routes
 });
 
 export default router
