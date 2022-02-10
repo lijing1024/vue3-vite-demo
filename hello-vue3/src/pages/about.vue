e37238b8c24478b5de1e756893c752bd715da4c0
lijing1024 <295119348@qq.com>
2022-02-10 17:00:40 +0800

    feat: add Heading.jsx

--- hello-vue3/src/pages/about.vue
+++ hello-vue3/src/pages/about.vue
@@ -6,11 +6,15 @@
 		</Rate>
 		<Rate v-model="score" theme="green" @update="update"></Rate>
 		<Rate v-model="score" theme="yellow" @update="update"></Rate>
+		<Heading :level="1">hello, 这里是一级标题</Heading>
+		<Heading :level="3">hello, 这里是三级标题</Heading>
 	</div>
 </template>
 
 <script setup>
 	import Rate from '../components/Rate.vue';
+	import Heading from '../components/Heading.jsx';
+	console.log(Heading);
 	import { ref } from 'vue';
 	let score = ref(3.5);
 	function update(num) {
