e7cad24df09490e302712fb9583455b82ddbd4be
lijing1024 <295119348@qq.com>
2022-02-10 18:20:38 +0800

    feat: add Element3

--- hello-vue3/src/main.js
+++ hello-vue3/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Element3 from 'element3'
+import 'element3/lib/theme-chalk/index.css'
 
 import router from './router/index.js';
 import store from './store/index.js';
@@ -7,4 +9,5 @@ import store from './store/index.js';
 createApp(App)
   .use(router)
   .use(store)
+  .use(Element3)
   .mount('#app')
