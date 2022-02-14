9249383efd489858cefc6ec892bfd48556250c33
lijing1024 <295119348@qq.com>
2022-02-14 15:01:00 +0800

    feat: add nprogress

--- hello-vue3/src/router/index.js
+++ hello-vue3/src/router/index.js
@@ -4,6 +4,9 @@ import {
 // import {
 //   createRouter, createWebHashHistory
 // } from './grouter/index.js';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
 import Home from '../pages/home.vue';
 import About from '../pages/about.vue';
 import Test from '../pages/test.vue';
@@ -30,5 +33,14 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes
 });
+
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  next()
+})
+
+router.afterEach(() => {
+  NProgress.done();
+})
 
 export default router
