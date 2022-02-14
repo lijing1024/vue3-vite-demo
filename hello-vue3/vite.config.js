c7d152d3d87b470edcba8574efd6a3e3754b6b87
lijing1024 <295119348@qq.com>
2022-02-14 15:52:44 +0800

    feat: 查看打包后文件体积，配置代码分割优化性能

--- hello-vue3/vite.config.js
+++ hello-vue3/vite.config.js
@@ -1,8 +1,45 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx';
+// 打包可视化
+import { visualizer } from 'rollup-plugin-visualizer'
+// 按需加载
+// import styleImport from 'vite-plugin-style-import'
+// import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()]
+  plugins: [vue(), vueJsx(), visualizer()],
+  // https://www.zhihu.com/question/498190531
+  css: {
+    postcss: {
+      plugins: [
+        {
+          postcssPlugin: 'internal:charset-removal',
+          AtRule: {
+            charset: (atRule) => {
+              if (atRule.name === 'charset') {
+                atRule.remove();
+              }
+            }
+          }
+        }
+      ],
+    },
+  },
+  // https://blog.csdn.net/weixin_41277748/article/details/116431789
+  base: '/dist/',
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 })
